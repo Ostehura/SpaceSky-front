03595ebc593faf12e399d856ebae5b7e3e269d73
Ostehura <74655634+Ostehura@users.noreply.github.com>
2025-11-19 11:47:12 +0100

    Add login and registration page

---
+++ src/app/auth/signup/page.tsx
@@ -0,0 +1,109 @@
+"use client";
+import { useState, FormEvent } from "react";
+import { Form, Button, Card, Alert } from "react-bootstrap";
+import axios, { AxiosError } from "axios";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+
+export default function SignInPage() {
+  const [email, setEmail] = useState<string>("");
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [password_confirmation, setConfirmation] = useState<string>("");
+  const [error, setError] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
+  const router = useRouter();
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setError("");
+    setLoading(true);
+
+    try {
+      const res = await axios.post("http://localhost:8000/api/register/", {
+        email,
+        username,
+        password,
+        password2: password_confirmation,
+      });
+      if (res.status % 100 == 2) {
+        router.push("/");
+      } else {
+        setError(res.statusText);
+      }
+    } catch (err) {
+      setError(
+        JSON.stringify((err as AxiosError).response?.data) ||
+          "Ooops! Something went wrong"
+      );
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <Card className="p-4 shadow mt-5" style={{ maxWidth: 400, margin: "auto" }}>
+      <Card.Body>
+        <h3 className="text-center mb-3">Sign In</h3>
+
+        {error && <Alert variant="danger">{error}</Alert>}
+
+        <Form onSubmit={handleSubmit}>
+          <Form.Group className="mb-3" controlId="formEmail">
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formEmail">
+            <Form.Label>Select your username</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Write your username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Enter your password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formPasswordConfirmation">
+            <Form.Label>Password Confirmation</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Confirm your password"
+              value={password_confirmation}
+              onChange={(e) => setConfirmation(e.target.value)}
+              required
+            />
+          </Form.Group>
+          <Button
+            variant="primary"
+            type="submit"
+            disabled={loading}
+            className="w-100"
+          >
+            {loading ? "Signing in..." : "Sign In"}
+          </Button>
+        </Form>
+      </Card.Body>
+      <Card.Body>
+        Already have an account? <Link href="/auth/login">Log in here</Link>
+      </Card.Body>
+    </Card>
+  );
+}
