fef590cdf16425cfdb2814afd8d22a921b922eaa
Ostehura <74655634+Ostehura@users.noreply.github.com>
2026-01-27 20:04:18 +0100

    add reading from env file

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: process.env.NEXT_PUBLIC_BACKEND,
 });
 
 api.interceptors.request.use(async (config) => {
